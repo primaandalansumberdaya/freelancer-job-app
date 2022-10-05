4c55b3daa8a3b67a56ee5fec6a4fb62e0b8baa33
Faqih996 <faqih.syakir11@gmail.com>
2022-10-05 23:59:22 +0700

    update

--- app/Http/Requests/Dashboard/Service/UpdateServiceRequest.php
+++ app/Http/Requests/Dashboard/Service/UpdateServiceRequest.php
@@ -27,24 +27,12 @@ class UpdateServiceRequest extends FormRequest
     public function rules()
     {
         return [
-             'title' => [
-                'required', 'string', 'max:255',
-            ],
-            'description' => [
-                'nullable', 'string', 'max:5000'
-            ],
-            'delivery_time' => [
-                'required', 'integer', 'max:100'
-            ],
-            'revision_limit' => [
-                'required', 'integer', 'max:100'
-            ],
-            'price' => [
-                'required', 'string',
-            ],
-            'note' => [
-                'nullable', 'string', 'max:5000'
-            ],
+            'title' => ['required', 'string', 'max:255'],
+            'description' => ['nullable', 'string', 'max:5000'],
+            'delivery_time' => ['required', 'integer', 'max:100'],
+            'revision_limit' => ['required', 'integer', 'max:100'],
+            'price' => ['required', 'string'],
+            'note' => ['nullable', 'string', 'max:5000'],
         ];
     }
 }
